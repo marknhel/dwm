eb94e1192477d9d102a1980474586c8c1917153b
Mark B <makibyet101@gmail.com>
2023-04-22 10:31:28 +0800

    modified spcmd

--- config.def.h
+++ config.def.h
@@ -4,8 +4,8 @@
 
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 10;       /* snap pixel */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 5;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
@@ -14,8 +14,8 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 10;       /* vertical padding of bar */
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 5;       /* vertical padding of bar */
+static const int sidepad            = 5;       /* horizontal padding of bar */
 static const char *fonts[]          = { "MesloLGS NF:size=10" };
 static const char dmenufont[]       = "MesloLGS NF:size=10";
 static const char col_gray1[]       = "#222222";
@@ -26,7 +26,7 @@ static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1,  col_gray2  },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_cyan  },
 //	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
@@ -37,7 +37,7 @@ typedef struct {
 const char *spcmd0[] = {TERMINAL, "-n", "spterm", "-g", "120x34", NULL };
 const char *spcmd1[] = {TERMINAL, "-n", "spdic", "-g", "144x41", "-e", "dic", NULL };
 const char *spcmd2[] = {TERMINAL, "-n", "spcalc", "-g", "144x41", "-e", "bc", "-q" , NULL };
-const char *spcmd3[] = {TERMINAL, "-n", "splf", "-g", "144x41", "-e", "lf", "~/", NULL };
+const char *spcmd3[] = {TERMINAL, "-n", "splf", "-g", "144x41", "-e", "lf", NULL };
 const char *spcmd4[] = {TERMINAL, "-n", "spwatch", "-g", "120x34", "-e", "watchv", NULL };
 const char *spcmd5[] = {TERMINAL, "-n", "spmusic", "-g", "145x35", "-e", "ncmpcpp", NULL };
 static Sp scratchpads[] = {
@@ -65,7 +65,7 @@ static const Rule rules[] = {
 	 */
 	/* class	instance	title		tags mask	isfloating	isterminal	noswallow	monitor */
 	{ "Gimp",	NULL,		NULL,		0,		1,		0,		0,		-1 },
-	{ "brave-browser",NULL,		NULL,		1 << 1,		0,		0,		-1,		-1 },
+	{ "Brave-browser",NULL,		NULL,		1 << 1,		0,		0,		-1,		-1 },
 //	{ "mpv",	NULL,		NULL,		0,		0,		0,		0,		1 },
 	{ TERMINAL,	NULL,		NULL,		0,		0,		1,		0,		-1 },
 	{ TERMINAL,	NULL,		"Event Tester",	0,		0,		0,		1,		-1 }, /* xev */
