9d57924ac7069ae81c8bf2dcefc41cc0c2dd7042
Mark B <makibyet101@gmail.com>
2023-09-10 18:54:53 +0800

    modified font size to 14 modified dmenu_run parameters

--- config.h
+++ config.h
@@ -16,8 +16,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
-static const char *fonts[]          = { "Iosevka Nerd Font:size=12" };
-static const char dmenufont[]       = "Iosevka Nerd Font:size=12";
+static const char *fonts[]          = { "Iosevka Nerd Font:size=14" };
+static const char dmenufont[]       = "Iosevka Nerd Font:size=14";
 static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#222222";
@@ -121,7 +121,8 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
 
 static const Key keys[] = {
